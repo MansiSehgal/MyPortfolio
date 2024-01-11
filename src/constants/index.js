44d7e5cada624f576a91911658e1ace1db5424df
Mansi Sehgal <mansisehgal22@gmail.com>
2024-01-11 19:05:28 +0530

    src code

---
+++ src/constants/index.js
@@ -0,0 +1,353 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  jsLogo,
+  textToSpeech,
+  sketchbook,
+  youtube,
+  todo,
+  sgfi,
+  randomuser,
+  movix,
+  ecommerce,
+  chatbot,
+  cravebite,
+  browser,
+  camera,
+  cooking,
+  essay,
+  frontend,
+  graphic,
+  link,
+  multimedia,
+  paintpalette,
+  socialmedia,
+  handshake,
+  ui,
+  speedometer,
+  durapid,
+  fsl,
+  nextJs,
+} from "../assets";
+
+export const overview =
+  "I am a passionate and detail-oriented front-end developer with a strong foundation in web technologies. My journey in the world of web development has been marked by hands-on experience, creativity, and a commitment to delivering visually appealing and intuitive user interfaces. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!";
+
+export const projectsDescription =
+  "Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively. I approach each project with a blend of creativity and functionality, ensuring that the user experience is not only visually engaging but also intuitive and seamless. My dedication to clean code and best practices reflects in the efficiency and maintainability of my projects.";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "experience",
+    title: "Work Experience",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Front-End Web Development",
+    icon: frontend,
+  },
+  {
+    title: "React Development",
+    icon: reactjs,
+  },
+  {
+    title: "UI/UX Design",
+    icon: ui,
+  },
+  {
+    title: "Creative Multimedia Integration",
+    icon: multimedia,
+  },
+  {
+    title: "API Integration",
+    icon: link,
+  },
+  {
+    title: "Performance Optimization",
+    icon: speedometer,
+  },
+  {
+    title: "Code Maintenance and Documentation",
+    icon: essay,
+  },
+  {
+    title: "Project Collaboration",
+    icon: handshake,
+  },
+  {
+    title: "Content Creation",
+    icon: multimedia,
+  },
+  {
+    title: "Web Designing",
+    icon: graphic,
+  },
+  {
+    title: "Photography",
+    icon: camera,
+  },
+  {
+    title: "Digital Presence",
+    icon: socialmedia,
+  },
+  {
+    title: "Artist",
+    icon: paintpalette,
+  },
+  {
+    title: "Cooking",
+    icon: cooking,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: jsLogo,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Next Js",
+    icon: nextJs,
+  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Associate Software Developer",
+    company_name: "Durapid Technologies Pvt. Ltd",
+    icon: durapid,
+    iconBg: "#E6DEDD",
+    date: "April 2022 - Present",
+    points: [
+      "Developed and maintained user interfaces using HTML5, CSS3, and JavaScript ES6, React.js, Redux Toolkit, TypeScript",
+      "Implemented state management with ReactJS and Redux, enhancing application performance.",
+      "Designed user interfaces using Material UI, Chakra UI, and Bootstrap, ensuring a seamless user experience.",
+      "Hands-on experience in developing and maintaining web applications.",
+      "Proficient in building responsive, user-friendly web applications using React.js, focusing on modular and reusable components.",
+      "Collaborated with UI/UX designers to create visually appealing and intuitive interfaces, ensuring seamless user experiences",
+      "Utilized Tailwind CSS and SCSS for efficient and scalable styling solutions.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Thank you, Mansi, for your commitment and always staying on the top of the work assigned to you. Mansi is our front-end team member from engineering. At a time, she is usually working on multiple unrelated items.She's always one of the first one to join the calls, comes prepared and makes sure that he gets answers to all his questions. He is always straight to the point which shows that he has thought through every possible scenario before and has come prepared.  His utmost dedication helps us in achieving the sprint goals on time. Also, all the work he does comes out with great quality that over the time he has gained everyone's trust. He knows what he is doing that even when you ask him about something specific related to his work even after weeks or months, he would put everything as if he is working on it now!. Want to express my gratitude to everything you do Mansi and hoping that you will keep the same passion and zeal towards your work and deliver your best!",
+    name: "Dimple Agarwal",
+    designation: "Director Product Management",
+    company: "Publicis Sapient",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "Mansi has been one team member who has scaled up pretty well and earned good trust from his peers. He has been involved in some of the key deliverables in Albertsons project, which he delivered with quality. He has shown utmost dedication and accountability towards the work assigned to him. Be it calling out dependencies in early stage or having discussion with business team for requirements, Mansi does a thorough job of understanding the task in hand and then executing it with great dedication. And whenever the track lead is not present Mansi has also stepped up and tried to full-fill the role, which is a great quality and will take him to the road of success. Thank you for your contribution. Keep doing the great work!",
+    name: "Gautam Chadha",
+    designation: "Senior Manager",
+    company: "Publicis Sapient",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "Mansi Sehgal helped us to refactor the FE logic of Rx status which had direct impact on application, and he was able to deliver the change on time with pro-active participation to understand the requirements behind it, reducing the margin of errors before our major release. Thank you Mansi for you dedication and hard work.",
+    name: "Ghanshyam Verma",
+    designation: "Lead Experience Engineer",
+    company: "Publicis Sapient",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Netflix Clone App",
+    description:
+      "A Netflix clone app using Next.js and MongoDB is a web application that replicates the core features and functionality of the popular streaming service, Netflix. Developed with Next.js for the frontend and MongoDB for the backend database, this clone aims to provide users with a similar streaming experience. It incliudes User Authentication, Content Catalog,Media Player,Search and Filtering,Watchlist and Favorites,Backend Integration with MongoDB",
+    tags: [
+      {
+        name: "NextJS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MongoDB API",
+        color: "green-text-gradient",
+      },
+    ],
+    image: movix,
+    hosted_link: "https://netflix-clone-ten-zeta-29.vercel.app/",
+    source_code_link: "https://github.com/MansiSehgal/Netflix-Clone",
+  },
+  {
+    name: "React E-commerce",
+    description:
+      "Created a fully functional e-commerce website using React, Redux, and Styled Components. The project included features like product listings, shopping cart functionality, and user authentication. The use of styled components ensured a visually appealing and responsive user interface.",
+    tags: [
+      {
+        name: "ReactJS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ecommerce,
+    hosted_link: "https://react-e-commerce-three-swart.vercel.app/",
+    source_code_link: "https://github.com/MansiSehgal/React-E-Commerce",
+  },
+  // {
+  //   name: "Chat Bot",
+  //   description:
+  //     "Developed a user-friendly Chat Bot utilizing vanilla JavaScript. The application engages users with an interactive front-end conversation. This project emphasizes creativity and interactivity in web development, showcasing the ability to implement dynamic and responsive chat interfaces",
+  //   tags: [
+  //     {
+  //       name: "HTML",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "CSS",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "JavaScript",
+  //       color: "green-text-gradient",
+  //     },
+  //   ],
+  //   image: chatbot,
+  //   hosted_link: "https://chat-bot-hazel.vercel.app/",
+  //   source_code_link: "https://github.com/1Mansigupta/ChatBot",
+  // },
+  // {
+  //   name: "Random User App",
+  //   description:
+  //     "Crafted a dynamic Random User application using React, seamlessly fetching data from external APIs. The project displayed adeptness in working with asynchronous operations, RESTful APIs, and rendering user information cards. The intuitive user interface offers a smooth and engaging experience.",
+  //   tags: [
+  //     {
+  //       name: "HTML",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "CSS",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "JavaScript",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "REST API",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: randomuser,
+  //   hosted_link: "https://random-user-gamma.vercel.app/",
+  //   source_code_link: "https://github.com/1Mansigupta/random-user",
+  // },
+  {
+    name: "To-Do App",
+    description:
+      "The React Chakra UI To-Do List is a simple and elegant task management application designed to help users organize and prioritize their daily activities. Built using React.js, a powerful JavaScript library for building user interfaces, and styled with Chakra UI, a modern React component library, this To-Do List offers a seamless and visually appealing user experience.",
+    tags: [
+      {
+        name: "ReactJS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Chakra-UI",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "green-text-gradient",
+      },
+    ],
+    image: todo,
+    hosted_link: "https://to-do-chakra-ui.vercel.app/",
+    source_code_link: "https://github.com/MansiSehgal/ToDO-ChakraUI",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
